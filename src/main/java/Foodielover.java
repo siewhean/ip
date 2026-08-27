2259b620bf5e577cac774d7472ea262e5ce3b6c9
siewhean <65327039+siewhean@users.noreply.github.com>
2026-08-27 17:20:49 +0800

    Completed iP Week 2

---
+++ src/main/java/Foodielover.java
@@ -0,0 +1,23 @@
+/**
+ * Entry point for the Foodielover chatbot application.
+ */
+public class Foodielover {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        String banner = " ______              _ _      _                            \n"
+                + "|  ____|            | (_)    | |                           \n"
+                + "| |__ ___   ___   __| |_  ___| | _____   _____ _ __        \n"
+                + "|  __/ _ \\ / _ \\ / _` | |/ _ \\ |/ _ \\ \\ / / _ \\ '__|       \n"
+                + "| | | (_) | (_) | (_| | |  __/ | (_) \\ V /  __/ |          \n"
+                + "|_|  \\___/ \\___/ \\__,_|_|\\___|_|\\___/ \\_/ \\___|_|          \n";
+
+        System.out.println(LINE);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Foodielover.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
